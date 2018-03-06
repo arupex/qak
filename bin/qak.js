f960b1bf3c4213c0b596a10647c97fa441ed487f
Dan Irwin <daniel.irwin@enernoc.com>
2018-03-05 20:36:39 -0500

    fix reference

--- bin/qak.js
+++ bin/qak.js
@@ -6,7 +6,7 @@ console.log('projects', projects);
 
 if(process.argv.length === 2) {
 
-    let table = require('../lib/table')(projects);
+    let table = require('../lib/projectTable')(projects);
 
     console.log(table);
 }
