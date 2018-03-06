f372b36b189d61f4b490616b398dceda4a5e1ff9
Dan Irwin <daniel.irwin@enernoc.com>
2018-03-05 21:12:18 -0500

    get it ready to run scripts!

--- bin/qak.js
+++ bin/qak.js
@@ -1,10 +1,73 @@
 #!/usr/bin/env node
 
+let args = process.argv
+    .filter(e => e.indexOf('=') !== -1)
+    .reduce((acc, value) => {
+        let kv = value.split('=');
+        acc[kv[0].replace(/^-*/g, '')] = kv[1];
+        return acc;
+    }, {});
+
 let projects = require('../lib/findProjects');
+let projectTable = require('../lib/projectTable');
+let table = require('table').table;
+let spawner = require('child_process').spawnSync;
 
-if(process.argv.length === 2) {
+function spawn(cmd, cwd){
+    spawner('npm', ['run', cmd], {
+        cwd : cwd,
+        env : Object.assign({}, process.env, args)
+    });
+}
 
-    let table = require('../lib/projectTable')(projects);
+let projectMapping = projects.reduce((acc, v) => {
+    acc[v.name] = v;
+    return acc;
+}, {});
 
-    console.log(table);
+let scriptMappings = projects.reduce((acc, v) => {
+
+    Object.keys(v.scripts).forEach(scriptName => {
+        if(!acc[scriptName]) {
+            acc[scriptName] = [];
+        }
+
+        acc[scriptName].push({
+           project : v.name,
+           scriptName : scriptName,
+           script : v.scripts[scriptName]
+        });
+    });
+    return acc;
+}, {});
+
+
+let project = projectMapping[process.argv.find( e => projectMapping[e])];
+let script = scriptMappings[process.argv.find( e => scriptMappings[e])];
+
+if(project && script) {
+    spawn(project.scripts[script.scriptName], project.cwd);
+}
+else {
+    if(project && !script) {
+        let table = projectTable([project]);
+        console.log(table);
+    }
+    else if(script && script.length === 1) {
+        let project = projectMapping[script[0].project];
+        spawn(script[0].scriptName, project.cwd);
+    }
+    else if(script && script.length > 1) {
+        let arrayOfArrays = ['Project', 'Command', 'Script'];
+        script.forEach(e => {
+            arrayOfArrays.push([e.project, e.scriptName, e.script]);
+        });
+        console.log(table(script));
+    }
+    else {
+
+        let table = projectTable(projects);
+        console.log(table);
+
+    }
 }
