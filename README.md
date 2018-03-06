c19986d94595bab12d9dd8bf7b96214db0b8aad5
Dan Irwin <daniel.irwin@enernoc.com>
2018-03-05 21:29:50 -0500

    trying to fix script table

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # qak
 A CLI for nested Projects
 
+[![npm version](https://badge.fury.io/js/qak.svg)](https://badge.fury.io/js/qak)
+[![dependencies](https://david-dm.org/arupex/qak.svg)](http://github.com/arupex/qak)
+![Build Status](https://api.travis-ci.org/arupex/qak.svg?branch=master) 
+![Donate]("https://www.patreon.com/bePatron?u=5407448)
+![lifetimeDownloadCount](https://img.shields.io/npm/dt/qak.svg?maxAge=35920000)
+
+
 ### Instalation
     npm i -g qak
     
--- bin/qak.js
+++ bin/qak.js
@@ -13,12 +13,12 @@ let projectTable = require('../lib/projectTable');
 let table = require('table').table;
 let spawner = require('child_process').spawnSync;
 
-function spawn(cmd, cwd){
+function spawn(cmd, cwd) {
     // call command and pass args via both args and env
-    spawner('npm', ['run', cmd].concat(Object.keys(args).map(e=> `--${e}=${args[e]}`) ), {
-        cwd : cwd,
-        env : Object.assign({}, process.env, args),
-        stdio : 'inherit'
+    spawner('npm', ['run', cmd].concat(Object.keys(args).map(e => `--${e}=${args[e]}`)), {
+        cwd: cwd,
+        env: Object.assign({}, process.env, args),
+        stdio: 'inherit'
     });
 }
 
@@ -30,41 +30,55 @@ let projectMapping = projects.reduce((acc, v) => {
 let scriptMappings = projects.reduce((acc, v) => {
 
     Object.keys(v.scripts).forEach(scriptName => {
-        if(!acc[scriptName]) {
+        if (!acc[scriptName]) {
             acc[scriptName] = [];
         }
 
         acc[scriptName].push({
-           project : v.name,
-           scriptName : scriptName,
-           script : v.scripts[scriptName]
+            project: v.name,
+            scriptName: scriptName,
+            script: v.scripts[scriptName]
         });
     });
     return acc;
 }, {});
 
 
-let project = projectMapping[process.argv.find( e => projectMapping[e])];
-let script = scriptMappings[process.argv.find( e => scriptMappings[e])];
+let project = projectMapping[process.argv.find(e => projectMapping[e])];
+let script = scriptMappings[process.argv.find(e => scriptMappings[e])];
 
-if(project && script) {
+if (project && script) {
     spawn(project.scripts[script.scriptName], project.cwd);
 }
 else {
-    if(project && !script) {
+    if (project && !script) {
         let table = projectTable([project]);
         console.log(table);
     }
-    else if(script && script.length === 1) {
+    else if (script && script.length === 1) {
         let project = projectMapping[script[0].project];
         spawn(script[0].scriptName, project.cwd);
     }
-    else if(script && script.length > 1) {
+    else if (script && script.length > 1) {
         let arrayOfArrays = [['Project', 'Command', 'Script']];
         script.forEach(e => {
             arrayOfArrays.push([e.project, e.scriptName, e.script]);
         });
-        console.log(table(arrayOfArrays));
+        let maxEach = process.stdout.columns - arrayOfArrays.reduce((acc, v) => Math.max(acc, v[0].length + v[1].length), 0) - 12;
+        console.log(table(arrayOfArrays, {
+            columns: {
+                0: {
+                    wrapWord: true
+                },
+                1: {
+                    wrapWord: true
+                },
+                2: {
+                    width: maxEach,
+                    wrapWord: true
+                }
+            }
+        }));
     }
     else {
 
